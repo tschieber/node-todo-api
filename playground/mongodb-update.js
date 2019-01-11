487bf7bbe1b36e87d3b41eb243322121e3dcacdc
Tim Schieber <tim.schieber@intouchsol.com>
2019-01-11 14:01:25 -0600

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,39 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').findOneAndUpdate({
+    //     _id: new ObjectID('5c38f05a1118da23a4db9414')
+    // }, {
+    //     $set: {
+    //         completed: true
+    //     }
+    // }, {
+    //     returnOriginal: false
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').findOneAndUpdate({
+        _id: new ObjectID('5c38c927c513930a150c31a7')
+    }, {
+        $set: {
+            name: 'Billy Bob'
+        },
+        $inc: {
+            age: 1
+        }
+    }, {
+        returnOriginal: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    //client.close();
+});
