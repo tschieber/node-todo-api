2d4a2e599785a660dd90994b511c670daf9988f4
Tim Schieber <tim.schieber@intouchsol.com>
2019-01-11 13:47:25 -0600

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,32 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').deleteMany({text: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // });
+
+    // db.collection('Todos').deleteOne({text: 'Eat lunch'}).then((result) => {
+    //     console.log(result);
+    // });
+
+    // db.collection('Todos').findOneAndDelete({completed: false}).then((result) => {
+    //     console.log(result);
+    // });
+
+    db.collection('Users').deleteMany({text: 'Tim'});
+
+    db.collection('Users').findOneAndDelete({
+        _id: new ObjectID("5c38f1921118da23a4db947a")
+    }).then((result) => {
+        console.log(JSON.stringify(result, undefined, 2));
+    });
+
+    //client.close();
+});
