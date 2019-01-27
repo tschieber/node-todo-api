4ecdfd407dd0908a44da4e17195e4424e8a90197
tschieber <tschieber@users.noreply.github.com>
2019-01-27 16:19:26 -0600

    Test the DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -65,7 +65,7 @@ app.delete('/todos/:id', (req, res) => {
         }
 
         // Success
-        res.send(todo);
+        res.send({todo});
     }).catch((e) => {
         res.status(400).send();
     });
